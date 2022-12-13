6c6495e492bd45d655fe0d76093e7ce72c4e9d3c
Vinicius Fernandes <vinicius.fernandes@diga.com.br>
2022-12-13 11:34:08 -0300

    remove token

--- src/http/index.ts
+++ src/http/index.ts
@@ -5,7 +5,6 @@ const Http = axios.create({
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'multipart/form-data',
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
     }
 })
 
