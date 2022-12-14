72a91cac0743b404a567ebf7de742b503dae3016
Vinicius Fernandes <vinicius.fernandes@diga.com.br>
2022-12-14 20:54:02 -0300

    button show password

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -29,9 +29,9 @@
         <div>
           <label for="password" class="sr-only">Senha</label>
           <div class="relative">
-            <input required v-model="form.password" type="password"
+            <input required v-model="form.password" :type="password"
               class="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm" placeholder="Informe sua senha" />
-            <span class="absolute inset-y-0 inline-flex items-center right-4">
+            <span @click="showPass" class="cursor-pointer absolute inset-y-0 inline-flex items-center right-4">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -57,8 +57,8 @@
           </p>
           <button type="submit"
             class="inline-flex justify-center px-5 py-3 ml-3 text-sm font-medium text-white bg-blue-500 rounded-lg">
-            <svg v-if="disabledClick" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 24 24">
+            <svg v-if="disabledClick" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
               <path class="opacity-75" fill="currentColor"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
@@ -86,11 +86,17 @@ fath(LockClosedIcon)
 export default defineComponent({
   name: 'LoginView',
   components: { LockClosedIcon, NavBarComponent },
+  methods: {
+    showPass() {
+      this.password == 'text' ? this.password = 'password' : this.password = 'text'
+    }
+  },
   data() {
     return {
       show_msg_login: sessionStorage.getItem('msg_login') ? true : false,
       registerLink: '/register',
-      forgotPassword: '/forgot-password'
+      forgotPassword: '/forgot-password',
+      password: 'password'
     }
   },
   setup() {
