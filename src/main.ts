443cb0440c4b71e6d42f801aa4a3037ce46b0dfd
Vinicius Fernandes <vinicius.fernandes@diga.com.br>
2022-12-19 11:29:17 -0300

    add refresh tokens

--- src/main.ts
+++ src/main.ts
@@ -9,26 +9,24 @@ import VueAnalytics from 'vue-analytics';
 
 // const loading = ref(true)
 
-// Http.interceptors.response.use(
-//     response => {
-//         setTimeout(() => {
-//             loading.value = false
-//         }, 500)
-
-//         return response
-//     },
-//     // error => {
-//     //     if (error.response.status == 401) {
-//     //         Http.post('/refresh')
-//     //             .then(response => {
-//     //                 console.log('=>' + response)
-//     //             }).catch(error => {
-//     //                 console.log('Error => ' + error)
-//     //             })
-//     //     }
-//     //     return error
-//     // }
-// )
+Http.interceptors.response.use(
+    response => {
+        return response
+    },
+    error => {
+        if (error.response.status == 401) {
+            console.log('ok')
+            Http.get('/refresh')
+                .then(response => {
+                    localStorage.setItem('token', response.data)
+                    location.reload()
+                }).catch(error => {
+                    console.log('Error => ' + error)
+                })
+        }
+        return error
+    }
+)
 
 // export default loading
 
